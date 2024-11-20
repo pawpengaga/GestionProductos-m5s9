68873d00eb26f259746476ff7a6b9905cbeef115
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-20 20:36:06 -0300

    tabla y servlet vacio

--- database.sql
+++ database.sql
@@ -2,3 +2,13 @@
 CREATE DATABASE m5_gestion;
 
 -- Creacion de las tablas
+CREATE TABLE public.productos
+(
+    id serial NOT NULL,
+    nombre character varying(50),
+    precio double precision,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.productos
+    OWNER to postgres;
