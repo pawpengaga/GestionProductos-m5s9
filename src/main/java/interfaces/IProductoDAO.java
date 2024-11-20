6b724e77a8ccfee95ef69acc9e64dbd7e4a42169
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-20 20:21:07 -0300

    GETALL listo

--- src/main/java/interfaces/IProductoDAO.java
+++ src/main/java/interfaces/IProductoDAO.java
@@ -1,5 +1,6 @@
 package interfaces;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import modelo.Producto;
@@ -7,8 +8,8 @@ import modelo.Producto;
 public interface IProductoDAO {
 
   public void addProduct(Producto p);
-  public List<Producto> getAllProducts();
-  public Producto getProducto(int id);
+  public List<Producto> getAllProducts() throws SQLException;
+  public Producto getProducto(int id) throws SQLException;
   public void updateProduct(Producto p);
   public void eliminarProduct(int id);
 
--- src/main/java/modelo/ProductoDAOImpl.java
+++ src/main/java/modelo/ProductoDAOImpl.java
@@ -2,7 +2,10 @@ package modelo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import interfaces.IProductoDAO;
@@ -36,15 +39,40 @@ public class ProductoDAOImpl implements IProductoDAO {
       } else {
         System.out.println("Ocurrio un fallo al agregar el producto...");
       }
+      // Cerramos el statement
+      stmt.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
   }
 
   @Override
-  public List<Producto> getAllProducts() {
-    return null;
+  public List<Producto> getAllProducts() throws SQLException {
+    List<Producto> productos = new ArrayList<>();
+    String sql = "SELECT * FROM productos";
 
+    // Nos conectamos y creamos el statement simple
+    Connection cnx = ConexionDB.getConnect();
+    Statement stmt = cnx.createStatement();
+
+    try {
+      // Creamos un objeto que reciba el select
+      // Esto se hace CADA VEZ que se usa un SELECT
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        // Ingresamos los nombres del objeto como vienen en el RESULTADO DE LA CONSULTA, considera los ALIAS de SQL
+        productos.add(new Producto(rs.getInt("id"), rs.getString("nombre"), rs.getDouble("precio")));
+      }
+      // Cerramos el resultset
+      rs.close();
+      stmt.close();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    // Retornamos la lista Java una vez llenada
+    return productos;
   }
 
   @Override
