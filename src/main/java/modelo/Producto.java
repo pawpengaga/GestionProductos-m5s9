f142b6e1737a5fae04d851059517cd961332a14d
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-20 19:54:54 -0300

    producto creado

---
+++ src/main/java/modelo/Producto.java
@@ -0,0 +1,41 @@
+package modelo;
+
+public class Producto {
+
+  private int idProducto;
+  private String nombre;
+  private double precio;
+
+
+  public Producto(int idProducto, String nombre, double precio) {
+    this.idProducto = idProducto;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+
+  public int getIdProducto() {
+    return this.idProducto;
+  }
+
+  public void setIdProducto(int idProducto) {
+    this.idProducto = idProducto;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public double getPrecio() {
+    return this.precio;
+  }
+
+  public void setPrecio(double precio) {
+    this.precio = precio;
+  }
+
+
+}
